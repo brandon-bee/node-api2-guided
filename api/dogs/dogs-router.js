7201c7bf1b34f6621d961f9c726b31409bf72cac
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-08 10:36:45 -0800

    GP solution

---
+++ api/dogs/dogs-router.js
@@ -0,0 +1,17 @@
+const Dog = require('./dogs-model');
+const router = require('express').Router();
+// DOGS ENDPOINTS
+router.get('/', (req, res) => {
+  Dog.find()
+    .then(dogs => {
+      res.status(200).json(dogs);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the dogs',
+      });
+    });
+});
+
+module.exports = router;
